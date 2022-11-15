a82ced05b33732c2293c79f090d20ecd3d99f3be
BM <bm1120836@yandex.ru>
2022-11-15 23:12:09 +0300

    interface.py

---
+++ interface.py
@@ -0,0 +1,20 @@
+
+# Модуль импортирования данных
+# Для работы с комплексными и рациональными числами возможно
+# импортировать классы fraction и cmath
+
+
+from fractions import Fraction
+import cmath
+
+def get_value():
+    value = input('Введите число')
+    try:
+        value = Fraction(value)
+        return value
+    except ValueError:
+        value = complex(value)
+        return value
+def get_operations():
+    operations = input('Введите операцию')
+    return operations
