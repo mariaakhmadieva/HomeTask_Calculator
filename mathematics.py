443391891c5ddbe29316e1f03412030165b1a80e
mariaakhmadieva <mariaakhmadieva@yandex.ru>
2022-11-16 17:03:27 +0500

    mathematics was added

---
+++ mathematics.py
@@ -0,0 +1,20 @@
+x = 0
+y = 0
+
+def init(a, b):
+    global x
+    global y
+    x = a
+    y = b
+
+def do_it(operation):
+    if operation == '+':
+        return x + y
+    if operation == '-':
+        return x - y
+    if operation == '*':
+        return x * y
+    if operation == '/':
+        return x / y
+    else:
+        return 'Incorrect data'
