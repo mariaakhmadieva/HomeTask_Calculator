4c38518b4086fe1bd28f484e67f2f9d9ab8afad8
Ksenia <ksenia.barteva@gmail.com>
2022-11-24 20:23:27 +0700

    Исключила записи с результатом None

--- logbook.py
+++ logbook.py
@@ -4,19 +4,21 @@
 from datetime import datetime
 
 def get_logbook(x, y, sign, result):
-    str_ = f'{x} {sign} {y} = {result};'
+    if result != None:
+        str_ = f'{x} {sign} {y} = {result};'
 
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open ('our_log.csv', 'a+') as log:
-        log.write(f'{now} \n{str_} ')
-        log.seek(0)
-        f1 = log.readlines()
-    with open ('our_log.csv', 'w+') as log:
-        if len(f1) > 20:
-            f2 = f1[-20:]
-            log.writelines(f2 + ['\n'])
-        else:
-            log.writelines(f1 + ['\n'])
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open ('our_log.csv', 'a+') as log:
+            log.write(f'{now} \n{str_} ')
+            log.seek(0)
+            f1 = log.readlines()
+        with open ('our_log.csv', 'w+') as log:
+            if len(f1) > 20:
+                f2 = f1[-20:]
+                log.writelines(f2 + ['\n'])
+            else:
+                log.writelines(f1 + ['\n'])
+
 
 
 
